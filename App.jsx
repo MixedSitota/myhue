c8cb0085e5d26d9f390e60b47c333164941fae9e
MixedSitota <nikitaworkneh@gmail.com>
2026-05-13 23:34:07 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,8 +2,8 @@ import { useState, useRef, useCallback, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 // ─── PASTE YOUR SUPABASE CREDENTIALS HERE ───────────────────────────────────
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://ktvdxnqubwrlosnkftqi.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0dmR4bnF1YndybG9zbmtmdHFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2OTQxMTIsImV4cCI6MjA5NDI3MDExMn0.U8BujYy5-dAYTsvLmtx9geCbBfYpl2s_x2wWMeypWNw";
 // ────────────────────────────────────────────────────────────────────────────
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
